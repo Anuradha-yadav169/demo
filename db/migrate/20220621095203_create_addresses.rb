c3b3f36a98ca0549f0332d98ceda932a7e432b72
Anuradha Yadav <you@example.com>
2022-06-21 15:25:58 +0530

    Add Addresses

---
+++ db/migrate/20220621095203_create_addresses.rb
@@ -0,0 +1,14 @@
+class CreateAddresses < ActiveRecord::Migration[6.0]
+  def change
+    create_table :addresses do |t|
+      t.string :name
+      t.string :address1
+      t.string :address2
+      t.integer :pincode
+      t.integer :city
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_082946) do
+ActiveRecord::Schema.define(version: 2022_06_21_095203) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
-    t.string "city"
-    t.string "full_name"
+    t.string "name"
     t.string "address1"
     t.string "address2"
     t.integer "pincode"
+    t.integer "city"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -71,6 +73,7 @@ ActiveRecord::Schema.define(version: 2022_06_20_082946) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
   add_foreign_key "addresses", "users"
   add_foreign_key "cart_items", "products"
   add_foreign_key "cart_items", "users"
