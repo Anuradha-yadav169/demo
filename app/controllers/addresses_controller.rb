960a9c82a0afa4817c89e9cf03b54fce5b856218
Anuradha Yadav <you@example.com>
2022-06-27 15:42:58 +0530

    Resolve changes in Address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,25 +1,21 @@
 class AddressesController < ApplicationController
   def index
-    if current_user.present?
-      #@addresses = current_user.addresses.find(params[:id]) 
+    if current_user.present? 
       @addresses = current_user.addresses
     end
 
   end
 
   def show
-    #@address = Address.find(params[:id])
-    @address = current_user.addresses.find(params[:id])  
+    @address = Address.find(params[:id]) 
   end
 
   def new
-    # @address = current_user.addresses.new 
-    @address = Addresses.new 
+    @address = current_user.addresses.new 
   end
 
   def create
-     #@address = current_user.addresses.new(address_params)
-    @address = Address.new(address_params) 
+    @address = current_user.addresses.new(address_params)
     if @address.save   
       redirect_to root_path
     else   
@@ -29,8 +25,7 @@ class AddressesController < ApplicationController
   end
 
   def edit
-    @address = current_user.addresses.find(params[:id])  
-    #@address = Address.find(params[:id])  
+      @address = current_user.addresses.find(params[:id])   
   end
 
   def update
@@ -57,24 +52,7 @@ class AddressesController < ApplicationController
 
   private
 
-  # def auth_user
-  #   @address = Address.find_by(id: params[:id])
-  #   unless params[:id].to_s == current_user.id.to_s
-  #     redirect_to root_path
-  #   end
-  # end
-
-
   def address_params
     params.require(:address).permit(:name, :pincode, :address1, :address2, :user_id)
   end
-
-
-  # def correct_user
-  #   @address = Address.find_by(id: params[:id])  // find the post
-  #   unless current_user?(@address.user)
-  #     redirect_to user_path(current_user)
-  #   end
-  # end
-
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
-   before_action :authenticate_user!
- 
- 
- 
-
-	protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -14,10 +10,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :surname, :email, :password)}
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :surname, :email, :password, :current_password)}
   end
-
-
-
-
-
-
 end
