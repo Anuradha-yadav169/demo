21f66eddbc8f0b2007b6b89714ff074f55324db2
Anuradha Yadav <you@example.com>
2022-06-27 12:51:31 +0530

    Resolved addresses issues

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,22 +1,25 @@
 class AddressesController < ApplicationController
   def index
     if current_user.present?
+      #@addresses = current_user.addresses.find(params[:id]) 
       @addresses = current_user.addresses
     end
+
   end
 
   def show
-    @addresses = current_user.addresses
-    #@adress = Address.find(params[:id])  
+    #@address = Address.find(params[:id])
+    @address = current_user.addresses.find(params[:id])  
   end
 
   def new
-    @address = current_user.addresses.new 
+    # @address = current_user.addresses.new 
+    @address = Addresses.new 
   end
 
   def create
-     @address = current_user.addresses.new(address_params)
-    # @address = Address.new(address_params) 
+     #@address = current_user.addresses.new(address_params)
+    @address = Address.new(address_params) 
     if @address.save   
       redirect_to root_path
     else   
@@ -26,11 +29,12 @@ class AddressesController < ApplicationController
   end
 
   def edit
-    @address = Address.find(params[:id])  
+    @address = current_user.addresses.find(params[:id])  
+    #@address = Address.find(params[:id])  
   end
 
   def update
-    @address = Address.find(params[:id])   
+    @address = current_user.addresses.find(params[:id])
     if @address.update_attributes(address_params)   
       flash[:notice] = 'Address updated!'   
       redirect_to root_path   
@@ -52,7 +56,25 @@ class AddressesController < ApplicationController
   end
 
   private
+
+  # def auth_user
+  #   @address = Address.find_by(id: params[:id])
+  #   unless params[:id].to_s == current_user.id.to_s
+  #     redirect_to root_path
+  #   end
+  # end
+
+
   def address_params
     params.require(:address).permit(:name, :pincode, :address1, :address2, :user_id)
   end
+
+
+  # def correct_user
+  #   @address = Address.find_by(id: params[:id])  // find the post
+  #   unless current_user?(@address.user)
+  #     redirect_to user_path(current_user)
+  #   end
+  # end
+
 end
