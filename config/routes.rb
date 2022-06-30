b124bef3abe498c8d52174564d086a7dde216dd7
Anuradha Yadav <you@example.com>
2022-06-30 16:33:06 +0530

    Add Product

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,9 @@
 Rails.application.routes.draw do
-  get 'adresses/index'
-  get 'adresses/show'
-  get 'adresses/new'
-  get 'adresses/create'
-  get 'adresses/edit'
-  get 'adresses/update'
-  get 'adresses/destroy'
+
   resources :addresses  
-  root 'addresses#index'  
-
-
+  
+  root 'products#index'
+  resources :products 
+ 
   devise_for :users
-  # root to: 'pages#index'
-  # root to: 'product#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
